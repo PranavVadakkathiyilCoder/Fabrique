4176a146fd035d09fbfdfbdf049fee5805bd8727
Pranav Vadakkathiyil <vpranav0801@gmail.com>
2025-05-26 16:33:46 +0530

    complete user Navbar

---
+++ client/src/layout/Footer.tsx
@@ -0,0 +1,132 @@
+import { Link } from "react-router-dom";
+import { FaTwitter,FaFacebook,FaInstagram ,FaGithub   } from "react-icons/fa";
+import RazoImg from '../assets/razorpay_img.png'
+
+const Footer = () => {
+ const footer = [
+  {
+    heading: "COMPANY",
+    links: [
+      { name: "About Us", path: "/about" },
+      { name: "Features", path: "/features" },
+      { name: "How It Works", path: "/how-it-works" },
+      { name: "Careers", path: "/careers" },
+    ],
+  },
+  {
+    heading: "HELP",
+    links: [
+      { name: "Customer Support", path: "/support" },
+      { name: "Delivery Details", path: "/shipping" },
+      { name: "Terms & Conditions", path: "/terms" },
+      { name: "Privacy Policy", path: "/privacy" },
+    ],
+  },
+  {
+    heading: "FAQ",
+    links: [
+      { name: "Manage Account", path: "/account" },
+      { name: "My Orders", path: "/orders" },
+      { name: "Return & Exchange", path: "/faq/returns" },
+      { name: "Payments", path: "/payments" },
+    ],
+  },
+  {
+    heading: "RESOURCES",
+    links: [
+      { name: "Free eBooks", path: "/resources/ebooks" },
+      { name: "Fashion Blog", path: "/blog" },
+      { name: "Style Guide", path: "/resources/style-guide" },
+      { name: "Size Charts", path: "/resources/size-chart" },
+    ],
+  },
+  {
+    heading: "FOLLOW US",
+    links: [
+      {
+        name: "Facebook",
+        path: "https://facebook.com",
+        icon: "fa-facebook-f",
+      },
+      {
+        name: "Instagram",
+        path: "https://instagram.com",
+        icon: "fa-instagram",
+      },
+      {
+        name: "Twitter",
+        path: "https://twitter.com",
+        icon: "fa-twitter",
+      },
+      {
+        name: "LinkedIn",
+        path: "https://linkedin.com",
+        icon: "fa-linkedin-in",
+      },
+    ],
+  },
+];
+const socialIcons = [
+  { icon: <FaTwitter />, path: "https://twitter.com" },
+  { icon: <FaFacebook />, path: "https://facebook.com" },
+  { icon: <FaInstagram />, path: "https://instagram.com" },
+  { icon: <FaGithub />, path: "https://github.com" },
+];
+
+
+
+  return (
+    <footer className="bg-[#f0f0f0]">
+       <section className="  p-4 sm:flex justify-evenly items-center">
+         <section>
+            <div className="flex flex-col items-center justify-center">
+              <p className="  footer-logo">FABRIQUE.CO</p>
+              <p className="">"Dress Bold. Shop Smart. Choose Fabrique."</p>
+              <ul className="flex gap-5 mt-2">
+                {
+                  socialIcons.map((data,index)=>(
+                    <Link key={index} to={data.path}>
+                      <li className="footer-icon">{data.icon}</li>
+                    </Link>
+                  )
+
+                  )
+                }
+                
+              </ul>
+            </div>
+        </section>
+        <section className="sm:flex gap-7  mt-3">
+            {footer.map((data,index)=>(
+                <div key={index} className=" ">
+                    <p className=" px-2 text-[1.2rem]  font-semibold py-1 sm:footer-content  ">{data.heading}</p>
+                   
+                    
+                        <ul className=" p-2 ">{data.links.map((info,index)=>(
+                            <li key={index} className="sm:footer-content hover:text-blue-600">
+
+                            <Link  to={info.path}>{info.name}</Link>
+                            
+                            
+                            </li>
+                            
+                        ))}</ul>
+                    
+                </div>
+            ))}
+        </section>
+       </section>
+        <section className="w-full flex justify-between items-center border-t-1 px-5 text-gray-400 text-[12px]">
+            <div>
+              <p>FABRIQUE.CO © 2025 All Rights Reserved</p>
+            </div>
+            <div className="flex items-center">
+              <p>Payment Partner</p>
+            <img src={RazoImg} alt="imgpay" width={"120px"} />
+            </div>
+        </section>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ client/src/layout/user/UserNav.tsx
@@ -0,0 +1,48 @@
+import { FiSearch } from "react-icons/fi";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdContact } from "react-icons/io"
+import { MdOutlineShoppingBag } from "react-icons/md"
+import { Link } from "react-router-dom";
+ 
+const navLinks = [
+  { name: "Home", path: "/" },
+  { name: "New Arrivals", path: "/new" },
+  { name: "Top Sellings", path: "/top-sellings" },
+  { name: "Accessories", path: "/accessories" },
+];
+
+const UserNav = () => {
+
+  return (
+    <nav className="w-screen">
+      <section className=" sm:text-[14px] text-[12px]  w-full bg-black text-gray-400 text-center">
+        <p>India's Fastest Online Shopping Destination <span className="text-gray-300 underline hover:text-gray-400">Shop Now</span></p>
+      </section>
+      <section className="flex w-full items-center justify-evenly py-3 border-b-1">
+        <p className="sm:hidden block"><GiHamburgerMenu className="text-2xl"/></p>
+        <p className="header-logo">FABRIQUE.CO</p>
+        <ul className="sm:flex gap-5 sm hidden">
+          {navLinks.map((link, index) => (
+    <li key={index} className="cursor-pointer text-gray-600 hover:text-black transition">
+      <Link to={link.path}>{link.name}</Link>
+    </li>
+  ))}
+        </ul>
+        <div className="sm:flex hidden items-center justify-between border px-6 h-10 w-96 rounded-full bg-gray-100">
+          <input type="text" placeholder="Get your Fashion Now " className="outline-none " />
+          <FiSearch className="text-3xl"/>
+          
+        </div>
+        <ul className="flex gap-4 items-center">
+          <li><FiSearch className="text-2xl sm:hidden "/></li>
+          <li className="flex relative"><MdOutlineShoppingBag className="text-2xl"/><span className="absolute -top-2 -right-2 bg-black text-white text-xs w-5 h-5 rounded-full flex items-center justify-center">
+      {4}
+    </span></li>
+          <li><IoMdContact className="text-2xl"/></li>
+        </ul>
+      </section>
+    </nav>
+  )
+}
+
+export default UserNav
